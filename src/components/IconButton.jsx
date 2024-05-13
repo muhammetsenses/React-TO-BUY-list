d07d542a121e4230b6f43d11df99d102530982f6
muhammetsenses <70330826+muhammetsenses@users.noreply.github.com>
2024-05-13 16:06:42 +0300

    IconButton.jsx

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,7 +1,18 @@
 import { BsTrash } from "react-icons/bs";
+import React from "react";
 
-const IconComp = () => {
-  return <BsTrash />;
+const IconComp = ({product,products,setProducts}) => {
+  const deleteProduct =(e) => {
+    e.stopPropagation();
+    console.log(product)
+    
+    const newProducts = products.filter(oldproduct => oldproduct.id !== product.id)
+    setProducts(newProducts)
+    console.log(newProducts)
+  }
+
+  return <button onClick={deleteProduct} style={{background: "red",color:"white"}}><BsTrash /></button>;
 };
 
 export default IconComp;
+
