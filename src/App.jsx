e8808782084e2402a75ceeb308f6d2e6310e8f05
muhammetsenses <70330826+muhammetsenses@users.noreply.github.com>
2024-05-13 16:06:10 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-
 import { Form, Button, Container, Table } from "react-bootstrap";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
-import styled from "styled-components";
-import { useEffect } from "react";
 import JSConfetti from "js-confetti";
 import IconComp from "./components/IconButton";
-
-const StyledTr = styled.tr`
-  text-decoration: ${(props) => (props.$isBought ? "line-through" : "none")};
-  cursor: pointer;
-`;
-
-const StyledTd = styled.td`
-  background-color: red !important;
-  color: white !important;
-  width: 35px;
-`;
+import Fuse from "fuse.js";
 
 function App() {
   const shops = [
@@ -38,20 +25,17 @@ function App() {
   const [productShop, setProductShop] = useState("");
   const [productCategory, setProductCategory] = useState("");
 
-  useEffect(() => {
-    const isItCompleted = products.length > 0 && products.every((product) => product.isBought === true)
-    if(isItCompleted){
-      alert("Tüm ürünler satın alındı.");
-      jsConfetti.addConfetti()
-    }
-  },[products])
+  const [filteredName, setFilteredName] = useState("");
+  const [filteredShopId, setFilteredShopId] = useState("");
+  const [filteredCategoryId, setFilteredCategoryId] = useState("");
+  const [filteredStatus, setFilteredStatus] = useState("");
 
   const addProduct = () => {
     const product = {
-      id: uuidv4(),
       name: productName,
       shop: productShop,
       category: productCategory,
+      id: uuidv4().slice(0, 5),
       isBought: false,
     };
     setProducts([...products, product]);
@@ -60,104 +44,192 @@ function App() {
     setProductCategory("");
   };
 
-  const completed = (product) => {
-    const newProducts = products.map((oldProduct) =>
-      oldProduct.id === product.id
-        ? { ...oldProduct, isBought: !oldProduct.isBought }
-        : oldProduct
+  const purchased = (productId) => {
+    const updatedProducts = products.map((product) =>
+      product.id === productId
+        ? { ...product, isBought: !product.isBought }
+        : product
     );
-    setProducts(newProducts);
-  };
-
-  const deleteProduct = (product, e) => {
-    e.stopPropagation(); 
-    const updatedProducts = products.filter(
-      (oldProduct) => oldProduct.id !== product.id
-    );
+    if (updatedProducts.every((upProducts) => upProducts.isBought === true)) {
+      alert("Alışveriş Tamamlandı");
+      jsConfetti.addConfetti();
+    }
     setProducts(updatedProducts);
   };
 
-  const jsConfetti = new JSConfetti()
- 
+  const jsConfetti = new JSConfetti();
+
+  const filteredProducts = products.filter((product) => {
+    let result = true;
+    const fuse = new Fuse(products, { keys: ["name"] });
+    const res = fuse.search(filteredName);
+
+    if (filteredName !== "" && !res.find((r) => r.item.id === product.id)) {
+      result = false;
+    }
+    if (filteredShopId !== "" && product.shop !== filteredShopId) {
+      result = false;
+    }
+    if (filteredCategoryId !== "" && product.category !== filteredCategoryId) {
+      result = false;
+    }
+    if (
+      filteredStatus !== "all" &&
+      ((product.isBought === true && filteredStatus !== "bought") ||
+        (product.isBought === false && filteredStatus !== "notBought"))
+    ) {
+      result =  false
+    }
+    return result
+  });
 
   return (
     <>
       <Container>
-        <Form className="form mb-4 d-flex align-items-center"
-        onSubmit={(e) => {
-          e.preventDefault();
-          if(!productName || !productShop || !productCategory){
-            alert("Lütfen boş alanları doldurunuz")
-          }else(addProduct()) ;
-          
-        }}>
-          <Form.Group className=" d-flex">
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (!productName || !productShop || !productCategory) {
+              alert("Lütfen boş alanları doldurunuz");
+            } else {
+              addProduct();
+            }
+          }}
+          className="d-flex mb-4"
+        >
+          <Form.Group className="w-100">
             <Form.Control
-              
-              type="text"
-              placeholder="Product"
               value={productName}
-              onChange={(e) => {
-                setProductName(e.target.value);
-              }}
-              
+              onChange={(e) => setProductName(e.target.value)}
+              type="text"
+              placeholder="Name"
             />
-            <Form.Select
-              value={productShop}
-              onChange={(e) => {
-                setProductShop(e.target.value);
-              }}
-              required
-            >
-              <option>Shop</option>
-              {shops.map((shop) => (
-                <option key={shop.id} value={shop.name}>
-                  {shop.name}
-                </option>
-              ))}
-            </Form.Select>
-            <Form.Select
-              value={productCategory}
-              onChange={(e) => {
-                setProductCategory(e.target.value);
-              }}
-              required
-            >
-              <option>Category</option>
-              {categories.map((category) => (
-                <option key={category.id} value={category.name}>
-                  {category.name}
-                </option>
-              ))}
-            </Form.Select>
-            <p></p>
           </Form.Group>
-          <Button type="submit"  variant="primary">
+          <Form.Select
+            value={productShop}
+            onChange={(e) => setProductShop(e.target.value)}
+          >
+            <option>Shop</option>
+            {shops.map((shop) => (
+              <option key={shop.id} value={shop.name}>
+                {shop.name}
+              </option>
+            ))}
+          </Form.Select>
+          <Form.Select
+            value={productCategory}
+            onChange={(e) => setProductCategory(e.target.value)}
+          >
+            <option>Category</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.name}>
+                {category.name}
+              </option>
+            ))}
+          </Form.Select>
+
+          <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
-        <Table>
+        <Form className="d-flex mb-4">
+          <Form.Group className="w-100">
+            <Form.Control
+              value={filteredName}
+              onChange={(e) => setFilteredName(e.target.value)}
+              type="text"
+              placeholder="Name"
+            />
+          </Form.Group>
+          <Form.Select
+            className="h-100"
+            value={filteredShopId}
+            onChange={(e) => setFilteredShopId(e.target.value)}
+          >
+            <option>Shop</option>
+            {shops.map((shop) => (
+              <option key={shop.id} value={shop.name}>
+                {shop.name}
+              </option>
+            ))}
+          </Form.Select>
+          <Form.Select
+            className="h-100"
+            value={filteredCategoryId}
+            onChange={(e) => setFilteredCategoryId(e.target.value)}
+          >
+            <option>Category</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.name}>
+                {category.name}
+              </option>
+            ))}
+          </Form.Select>
+          <Form.Group
+            onChange={(e) => {
+              setFilteredStatus(e.target.value);
+            }}
+            className="mb-3 d-flex align-items-center"
+          >
+            <Form.Check
+              inline
+              value={"all"}
+              label="All"
+              name="group1"
+              type={"radio"}
+              id={`radio-1`}
+            />
+            <Form.Check
+              inline
+              value={"bought"}
+              label="Bought"
+              name="group1"
+              type={"radio"}
+              id={`radio-2`}
+            />
+            <Form.Check
+              inline
+              value={"notBought"}
+              name="group1"
+              label="Not Bought"
+              type={"radio"}
+              id={`radio-3`}
+            />
+          </Form.Group>
+        </Form>
+
+        <Table striped bordered hover>
           <thead>
             <tr>
               <th>Name</th>
               <th>Shop</th>
               <th>Category</th>
-              <th>Id</th>
+              <th>ID</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
-            {products.map((product) => (
-              <StyledTr
-                onClick={() => completed(product)}
-                $isBought={product.isBought}
+            {filteredProducts.map((product) => (
+              <tr
+                onClick={() => purchased(product.id)}
                 key={product.id}
+                style={{
+                  textDecoration:
+                    product.isBought === true ? "line-through" : "unset",
+                }}
               >
                 <td>{product.name}</td>
                 <td>{product.shop}</td>
                 <td>{product.category}</td>
                 <td>{product.id}</td>
-                <StyledTd onClick={(e) => deleteProduct(product, e)}><IconComp /></StyledTd>
-              </StyledTr>
+                <td>
+                  <IconComp
+                    product={product}
+                    products={products}
+                    setProducts={setProducts}
+                  />
+                </td>
+              </tr>
             ))}
           </tbody>
         </Table>
